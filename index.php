bf7eefbd2499752a8d5d7b7cfdb2e659e4e965d3
mayank-kr-verma <themayankverma@gmail.com>
2018-10-14 15:07:53 +0530

    first page is finally up

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>MakX was here</title>
-	</head>
-	<body>
-		<center><h1>Whoa! Lots of empty space here. Gotta put some stuff for display.</h1></center>
-	</body>
-</html>
